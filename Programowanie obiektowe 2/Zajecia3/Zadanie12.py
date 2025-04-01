b03a2ff2d981479a96ca24b9abe11ec1af8ccedf
Jakub Kałuża <j_kaluza@onet.pl>
2025-04-01 20:09:26 +0200

    Zadanie 12

--- Programowanie obiektowe 2/Zajecia3/Zadanie12.py
+++ Programowanie obiektowe 2/Zajecia3/Zadanie12.py
@@ -1,12 +1,13 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QWidget
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QWidget, QStatusBar
 from PySide6.QtGui import QAction, QIcon, QKeySequence
-
+from PySide6.QtCore import QSize, Qt
 
 class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
 
+    self.setWindowTitle("Learning App")
     centralWidget = QWidget()
     self.setCentralWidget(centralWidget)
 
@@ -62,30 +63,51 @@ class MainWindow(QMainWindow):
     actionsMenu.addSeparator()
     actionsMenu.addAction(closeAction)
 
+    # Tworzenie toolbara - Zadanie 2
+    toolbar = self.addToolBar("Math Toolbar")
+    toolbar.setOrientation(Qt.Vertical)
+    toolbar.setStyleSheet("border-right: 1px solid black; padding: 4px;")
+    toolbar.setIconSize(QSize(16,16))
+    toolbar.addAction(addOneAction)
+    toolbar.addSeparator()
+    toolbar.addAction(subOneAction)
+    toolbar.addSeparator()
+    toolbar.addAction(multiplyAction)
+    toolbar.addSeparator()
+    toolbar.addAction(halveAction)
+    toolbar.addSeparator()
+    toolbar.addAction(resetAction)
+    toolbar.addSeparator()
+    toolbar.addAction(closeAction)
 
+    statusBar = QStatusBar(self)
+    statusBar.setStyleSheet("background-color: lightgray;")
+    self.setStatusBar(statusBar)
 
     self.wynik = 0
     self.wynikLabel = QLabel(f"Wynik: {self.wynik}")
 
 
     layout = QGridLayout()
-    layout.addWidget(self.wynikLabel, 1, 1)
+    layout.setContentsMargins(0, 10, 10, 10)
+    layout.addWidget(toolbar, 0, 0, 3, 1)
+    layout.addWidget(self.wynikLabel, 1, 2)
 
     addButton = QPushButton("+1")
     addButton.clicked.connect(self.addOne)
-    layout.addWidget(addButton, 0, 0)
+    layout.addWidget(addButton, 0, 1)
 
     substractButton = QPushButton("-1")
     substractButton.clicked.connect(self.subtractOne)
-    layout.addWidget(substractButton, 0, 2)
+    layout.addWidget(substractButton, 0, 3)
 
     multiplyButton = QPushButton("x2")
     multiplyButton.clicked.connect(self.doubleResult)
-    layout.addWidget(multiplyButton, 2, 0)
+    layout.addWidget(multiplyButton, 2, 1)
 
     divideButton = QPushButton("/2")
     divideButton.clicked.connect(self.halveResult)
-    layout.addWidget(divideButton, 2, 2)
+    layout.addWidget(divideButton, 2, 3)
 
     centralWidget.setLayout(layout)
 
@@ -116,7 +138,7 @@ def main():
   app = QApplication(sys.argv)
   window = MainWindow()
   window.show()
-  sys.exit(app.exec_())
+  sys.exit(app.exec())
 
 
 if __name__ == "__main__":
